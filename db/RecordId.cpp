e68c3baa76a56d3ca25f2f46a3ea427fcc1857e2
Claire Fan <55124863+jfan318@users.noreply.github.com>
2023-10-03 00:10:04 -0400

    recordID passed test

--- db/RecordId.cpp
+++ db/RecordId.cpp
@@ -9,18 +9,28 @@ using namespace db;
 
 // TODO pa1.4: implement
 RecordId::RecordId(const PageId *pid, int tupleno) {
+    this->pid = pid;
+    this->tupleno = tupleno;
 }
 
 bool RecordId::operator==(const RecordId &other) const {
     // TODO pa1.4: implement
+    if (this->getPageId()->getTableId() == other.getPageId()->getTableId()
+    && this->getPageId()->pageNumber() == this->getPageId()->pageNumber()
+    && this->getTupleno() == other.getTupleno()){
+        return true;
+    }
+    return false;
 }
 
 const PageId *RecordId::getPageId() const {
     // TODO pa1.4: implement
+    return this->pid;
 }
 
 int RecordId::getTupleno() const {
     // TODO pa1.4: implement
+    return this->tupleno;
 }
 
 //
@@ -29,4 +39,7 @@ int RecordId::getTupleno() const {
 
 std::size_t std::hash<RecordId>::operator()(const RecordId &r) const {
     // TODO pa1.4: implement
+    std::size_t h1 = std::hash<int>{}(r.getPageId()->getTableId() ^ r.getPageId()->pageNumber());
+    std::size_t h2 = std::hash<int>{}(r.getTupleno());
+    return h1 ^ h2;
 }
